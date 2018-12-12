9782145c372a51075fd24261bd17ce780c736e22
fankaljead <728678732@qq.com>
2018-12-12 12:07:02 +0800

    add org test

---
+++ main.go
@@ -0,0 +1,18 @@
+/* ====================================================
+#   Copyright (C)2018 All rights reserved.
+#
+#   Author        : fankaljead
+#   Email         : fankaljead@gmail.com
+#   File Name     : main.go
+#   Last Modified : 2018-12-12 09:18
+#   Describe      : Main.go for test
+#
+# ====================================================*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
