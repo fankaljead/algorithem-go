bf133ad9e2b1b0077b396d1b20990d82101f90e8
fankaljead <728678732@qq.com>
2018-12-17 14:00:37 +0800

    添加求二项式公式的备忘录方法

--- curriculum-design/basic/binomial_expression.go
+++ curriculum-design/basic/binomial_expression.go
@@ -13,32 +13,50 @@ package basic
 
 import (
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 // CalculateBinomialRecursion is to calculate the binomial express parse n and k with recursion method
+// 计算二项式公式 使用递归
 func CalculateBinomialRecursion(k, n int) int {
-	if k == 1 && n == 1 {
-		return 1
-	} else if k == 1 {
-		return n
-	} else {
-		return CalculateBinomialRecursion(k-1, n-1) + CalculateBinomialRecursion(k, n-1)
+	if k > 0 && n > 0 {
+		if k == 1 && n == 1 {
+			return 1
+		} else if k == 1 {
+			return n
+		} else {
+			return CalculateBinomialRecursion(k-1, n-1) + CalculateBinomialRecursion(k, n-1)
+		}
 	}
+	return 0
 }
 
 // CalculateBinomialMemo is to calculate the binomial express parse n and k with memory method
+// 计算二项式公式 使用备忘录方法
 func CalculateBinomialMemo(k, n int) int {
-	if k == 1 && n == 1 {
-		return 1
-	} else if k == 1 {
-		return n
-	} else {
-		return CalculateBinomialRecursion(k-1, n-1) + CalculateBinomialRecursion(k, n-1)
+	if k > 0 && n > 0 {
+
+		if k == 1 && n == 1 {
+			return 1
+		} else if k == 1 {
+			return n
+		} else if cknv[strconv.Itoa(k)+","+strconv.Itoa(n)] == 0 {
+			cknv[strconv.Itoa(k)+","+strconv.Itoa(n)] = CalculateBinomialRecursion(k-1, n-1) + CalculateBinomialRecursion(k, n-1)
+		}
+		return cknv[strconv.Itoa(k)+","+strconv.Itoa(n)]
 	}
+	return 0
+}
+
+// CalculateBinomialIteration is to calculate the binomial express parse n and k with iteration method
+// 计算二项式公式 使用迭代方法
+func CalculateBinomialIteration(k, n int) int {
+	return 0
 }
 
 // CalculateBinomialBrute to brute force to calculate binomial express k, n
+// 计算二项式公式 使用数学方法
 func CalculateBinomialBrute(k, n int) int {
 	denomenator := 1
 	member := 1
@@ -52,6 +70,7 @@ func CalculateBinomialBrute(k, n int) int {
 }
 
 // GetKN return k, n, k < n <20
+// 随机生成 k, v, k < n < 20
 func GetKN() (k, n int) {
 	rand.Seed(time.Now().Unix())
 	k = rand.Intn(15) + 1
@@ -63,4 +82,10 @@ func GetKN() (k, n int) {
 	}
 
 	return
+}
+
+var cknv map[string]int
+
+func init() {
+	cknv = make(map[string]int)
 }
--- curriculum-design/basic/binomial_expression_test.go
+++ curriculum-design/basic/binomial_expression_test.go
@@ -14,19 +14,37 @@ package basic
 import (
 	"log"
 	"testing"
-	"time"
 )
 
 // CalculateBinomialRecursionTest 测试二项式公式的计算 使用递归
 func TestCalculateBinomialRecursion(t *testing.T) {
-	for i := 10; i > 0; i-- {
-		k, n := GetKN()
-		brute := CalculateBinomialBrute(k, n)
-		recursion := CalculateBinomialBrute(k, n)
-		log.Printf("C(%d, %d)=CalculateBinomialBrute=%d, CalculateBinomialRecursion=%d\n", k, n, brute, recursion)
-		if brute != recursion {
-			t.Error()
-		}
-		time.Sleep(time.Duration(1000) * time.Millisecond)
+	k, n := GetKN()
+	brute := CalculateBinomialBrute(k, n)
+	recursion := CalculateBinomialRecursion(k, n)
+	log.Printf("C(%d, %d)=CalculateBinomialBrute=%d, CalculateBinomialRecursion=%d\n", k, n, brute, recursion)
+	if brute != recursion {
+		t.Error()
+	}
+}
+
+// TestCalculateBinomialMemo测试二项式公式的计算 使用备忘录方法
+func TestCalculateBinomialMemo(t *testing.T) {
+	k, n := GetKN()
+	brute := CalculateBinomialBrute(k, n)
+	recursion := CalculateBinomialMemo(k, n)
+	log.Printf("C(%d, %d)=CalculateBinomialBrute=%d, CalculateBinomialMemo=%d\n", k, n, brute, recursion)
+	if brute != recursion {
+		t.Error()
+	}
+}
+
+// TestCalculateBinomialIteration 测试二项式公式的计算 使用备忘录方法
+func TestCalculateBinomialIteration(t *testing.T) {
+	k, n := GetKN()
+	brute := CalculateBinomialBrute(k, n)
+	recursion := CalculateBinomialIteration(k, n)
+	log.Printf("C(%d, %d)=CalculateBinomialBrute=%d, CalculateBinomialIteration =%d\n", k, n, brute, recursion)
+	if brute != recursion {
+		t.Error()
 	}
 }
