a21648692cc97ee650448357cf849c1bc98f2aa0
fankaljead <728678732@qq.com>
2018-12-23 20:54:59 +0800

    添加积分商品一次选择和多次选择的情况

--- curriculum-design/dynamic/max_value.go
+++ curriculum-design/dynamic/max_value.go
@@ -12,18 +12,33 @@
 package dynamic
 
 import (
-// "log"
+	"log"
 )
 
 // FindMaxValue 找到给定积分所能够获得的最大价值
 // prices 商品价格列表, credits 商品所需积分列表, exchangeRates 商品积分价格兑换比率, totalCredit 总积分
 // 返回商品最大价值时选中的商品列表下标
-func FindMaxValue(prices, credits []float64, totalCredit float64) (float64, *[]int) {
-	maxValue := float64(0)
+// 每种商品可以选择多次
+func FindMaxValue1(prices, credits []float64, totalCredit float64) (float64, *[]float64, *[]float64) {
 
+	weights := new([]float64) // 每个商品可以选择多次
+	values := new([]float64)
+	maxValue := float64(0)
+	resultValues := new([]float64)
+	resultWeights := new([]float64)
+
+	for i := 0; i < len(prices); i++ {
+		for j := 0; j <= int(totalCredit/credits[i]); j++ {
+			*weights = append(*weights, credits[i])
+			*values = append(*values, prices[i])
+		}
+	}
+
+	log.Println("weights length:", len(*weights))
+	log.Println("weights :", (*weights))
 	f := new([][]float64)
 
-	for i := 0; i < len(prices)+1; i++ {
+	for i := 0; i < len(*values)+1; i++ {
 		tf := new([]float64)
 		for j := 0; j < int(totalCredit)+1; j++ {
 			*tf = append(*tf, -1)
@@ -33,14 +48,52 @@ func FindMaxValue(prices, credits []float64, totalCredit float64) (float64, *[]i
 
 	result := new([]int)
 
+	tracebackMemo(len(*values), int(totalCredit), weights, values, f, result)
+	// tracebackMemo(len(prices), int(totalCredit), &credits, &prices, f, result)
+
+	for _, v := range *result {
+		maxValue += (*values)[v]
+	}
+
+	for _, v := range *result {
+		*resultWeights = append(*resultWeights, (*weights)[v])
+		*resultValues = append(*resultValues, (*values)[v])
+	}
+
+	return maxValue, resultWeights, resultValues
+}
+
+// FindMaxValue2 找到给定积分所能够获得的最大价值
+// prices 商品价格列表, credits 商品所需积分列表, exchangeRates 商品积分价格兑换比率, totalCredit 总积分
+// 返回商品最大价值时选中的商品列表下标
+// 每种商品只能选择一次
+func FindMaxValue2(prices, credits []float64, totalCredit float64) (float64, *[]int) {
+	maxValue := float64(0)
+
+	f := new([][]float64)
+
+	for i := 0; i < len(prices)+1; i++ {
+		tf := new([]float64)
+		for j := 0; j < int(totalCredit)+1; j++ {
+			*tf = append(*tf, -1)
+
+		}
+		*f = append(*f, *tf)
+
+	}
+
+	result := new([]int)
+
 	// tracebackMemo(len(prices), int(totalCredit), weights, values, f)
 	tracebackMemo(len(prices), int(totalCredit), &credits, &prices, f, result)
 
 	for _, v := range *result {
 		maxValue += prices[v]
+
 	}
 
 	return maxValue, result
+
 }
 
 func knapsackMemo(i, j int, weights, values *[]float64, f *[][]float64) float64 {
--- curriculum-design/dynamic/max_value_test.go
+++ curriculum-design/dynamic/max_value_test.go
@@ -22,10 +22,21 @@ func TestFindMaxValue(t *testing.T) {
 	credits := [...]float64{5000, 1000, 2500, 500, 3000, 1000, 2000, 1200} // 商品所需积分列表
 	totalCredit := float64(10000)                                          // 总积分
 
-	maxValue, result := FindMaxValue(prices[:], credits[:], totalCredit)
+	maxValue, resultWeights, resultValues := FindMaxValue1(prices[:], credits[:], totalCredit)
+	log.Println("考虑每种商品可以选择多次的情况")
 	log.Println("总积分为:", totalCredit)
 	log.Println("商品价格列表为: ", prices)
 	log.Println("商品所需积分为: ", credits)
-	log.Println("最大价值列表: ", *result)
+	log.Println("最大价值列表商品价格: ", *resultWeights)
+	log.Println("最大价值列表商品积分: ", *resultValues)
+	log.Println("最大价值为: ", maxValue)
+	log.Println()
+
+	maxValue, result := FindMaxValue2(prices[:], credits[:], totalCredit)
+	log.Println("考虑每种商品只能选择一次的情况")
+	log.Println("总积分为:", totalCredit)
+	log.Println("商品价格列表为: ", prices)
+	log.Println("商品积分列表为: ", credits)
+	log.Println("最大价值列表商品选择的为: ", *result)
 	log.Println("最大价值为: ", maxValue)
 }
