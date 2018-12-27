8278a6e25bed05628aeafb5fff315169aa327e46
fankaljead <728678732@qq.com>
2018-12-28 00:25:30 +0800

    校园选址

--- curriculum-design/extra/supermarket_location.go
+++ curriculum-design/extra/supermarket_location.go
@@ -11,27 +11,82 @@
 
 package extra
 
-//FindSupermarketLocation 找到超市位置
-func FindSupermarketLocation(matrix [][]int) (position, length int) {
+import (
+	"log"
+)
 
-	result := new([]int)
+// FindSupermarketLocation 找到超市位置
+// matrix 各个点到其它点的距离 pNumber 每个点到超市的人次
+func FindSupermarketLocation(matrix [][]int, pNumber []int) (position, length int, path [][]int) {
 
+	// 点的下标 -> 路径
+	//resultMap := make(map[int][][]int)
+
+	// 遍历每个点
 	for i := 0; i < len(matrix); i++ {
-		sum := 0
+		path := new([][]int)
+		selfToSelf := new([]int)
+		*selfToSelf = append(*selfToSelf, i) // 自己到自己
+		*path = append(*path, *selfToSelf)
+
+		for j := 0; j < len(matrix); j++ {
+			pathToJ := findShortestPath(i, j, matrix)
+			*path = append(*path, *pathToJ)
+		}
+	}
+
+	matrixS, resultMap := findShortest(matrix)
+
+	log.Println(matrixS)
+	log.Println(resultMap)
+	return
+}
+
+// 找到最短路径
+func findShortest(matrix [][]int) (matrixS *[][]int, pPath *map[int][][]int) {
+
+	matrixS = new([][]int)
+	path := make(map[int][][]int)
+	// 复制原数组
+	for i := 0; i < len(matrix); i++ {
+		tP := new([]int)
 		for j := 0; j < len(matrix[i]); j++ {
-			if j != i {
-				sum += matrix[i][j]
+			*tP = append(*tP, matrix[i][j])
+		}
+		*matrixS = append(*matrixS, *tP)
+	}
+
+	// 找出最短路径
+	for k := 0; k < len(*matrixS); k++ {
+		kA := new([][]int)
+		for i := 0; i < len(*matrixS); i++ {
+			kTi := new([]int)
+			for j := 0; j < len(*matrixS); j++ {
+				minP, po := min((*matrixS)[i][j], (*matrixS)[i][k]+(*matrixS)[k][j])
+				(*matrixS)[i][j] = minP
+				if po == 1 {
+					*kTi = append(*kTi, j)
+				} else {
+					*kTi = append(*kTi, k)
+				}
 			}
+			*kA = append(*kA, *kTi)
 		}
-		*result = append(*result, sum)
+		path[k] = *kA
 	}
+	pPath = &path
 
-	maxIndex := 0
-	for k, v := range *result {
-		if v > (*result)[maxIndex] {
-			maxIndex = k
-		}
+	return matrixS, pPath
+}
+
+func min(a, b int) (int, int) {
+	if a > b {
+		return b, 2
 	}
+	return a, 1
+}
 
-	return maxIndex, (*result)[maxIndex]
+func findShortestPath(i, j int, matrix [][]int) *[]int {
+	path := new([]int)
+	return path
 }
--- curriculum-design/extra/supermarket_location_test.go
+++ curriculum-design/extra/supermarket_location_test.go
@@ -18,22 +18,22 @@ import (
 
 func TestFindSupermarketLocation(t *testing.T) {
 	matrix := new([][]int)
+	pNumber := new([]int)
 
-	p1 := [...]int{0, 2, 3, 0, 5}
-	p2 := [...]int{1, 2, 3, 0, 5}
-	p3 := [...]int{0, 2, 3, 0, 5}
-	p4 := [...]int{4, 2, 3, 0, 5}
-	p5 := [...]int{2, 2, 3, 0, 5}
+	p1 := [...]int{0, 9999, 3, 9999}
+	p2 := [...]int{2, 0, 9999, 9999}
+	p3 := [...]int{9999, 7, 0, 1}
+	p4 := [...]int{6, 9999, 9999, 0}
 
 	*matrix = append(*matrix, p1[:])
 	*matrix = append(*matrix, p2[:])
 	*matrix = append(*matrix, p3[:])
 	*matrix = append(*matrix, p4[:])
-	*matrix = append(*matrix, p5[:])
 
 	log.Println("地址信息如下:", *matrix)
-	position, maxLength := FindSupermarketLocation(*matrix)
+	position, maxLength, path := FindSupermarketLocation(*matrix, *pNumber)
 	log.Println("超市应当建立在:", position)
 	log.Println("超市到各个地方总距离为:", maxLength)
+	log.Println("超市到各个地方总距离为:", path)
 
 }
